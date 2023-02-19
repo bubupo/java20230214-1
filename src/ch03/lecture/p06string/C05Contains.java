4ded6098a082e4e525459dc39575a13a9449d3f0
bubupo <yh0011223@gmail.com>
2023-02-19 19:06:00 +0900

    20190219

---
+++ src/ch03/lecture/p06string/C05Contains.java
@@ -0,0 +1,22 @@
+package ch03.lecture.p06string;
+
+public class C05Contains {
+	public static void main(String[] args) {
+		//contains
+		//특정 문자열이 있는 지 확인
+		// return type : boolean
+		
+		String str1 = """
+				동해물과 백두산이
+				마르고 닳도록
+				""";
+		boolean res1 = str1.contains("해물");
+		System.out.println(res1);
+		//중간에 이 단어가 있느냐
+		boolean res2 = str1.contains("해산물");
+		System.out.println(res2);
+		
+		
+		
+	}
+}
