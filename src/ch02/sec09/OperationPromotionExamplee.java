5abbaa5b0bd6792166875957366a4b06c0a92539
bubupo <123609467+bubupo@users.noreply.github.com>
2023-02-20 17:23:00 +0900

    20230220

--- src/ch02/sec09/OperationPromotionExamplee.java
+++ src/ch02/sec09/OperationPromotionExamplee.java
@@ -1,6 +1,6 @@
 package ch02.sec09;
 
-public class OperationPromotionExample {
+public class OperationPromotionExamplee {
 	public static void main(String[] args) {
 		byte result1 = 10 +20;			//컴파일 단계에서 연산
 		System.out.println("result1 : " + result1);
