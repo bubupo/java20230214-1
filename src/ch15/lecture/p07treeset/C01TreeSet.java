4f1ce59856ea2d57530358198268c58c732b41bf
bubupo <123609467+bubupo@users.noreply.github.com>
2023-03-16 17:15:11 +0900

    20230316

---
+++ src/ch15/lecture/p07treeset/C01TreeSet.java
@@ -0,0 +1,14 @@
+package ch15.lecture.p07treeset;
+
+import java.util.*;
+
+public class C01TreeSet {
+	Set<Integer> set = new HashSet<>();
+	Set<Integer> treeSet = new TreeSet<>();
+	
+	set.add(3000);
+	treeSet.add(3000);
+	set.add(20);
+	
+	
+}
